9ca1782bae9a59d7fb89bd705996945df18c6366
edris <edrissharoq@gmail.com>
2022-06-23 10:21:11 -0700

    this is capston

---
+++ src/Capstone/CapstoneProject.java
@@ -0,0 +1,19 @@
+package Capstone;
+
+public class CapstoneProject {
+
+	public static void main(String[] args) {
+		double a = 25.6;
+		double b = 58.35;
+		double c = a * b;
+		System.out.println(c);
+	}
+	double a = 25.6;
+	double b = 58.35;
+	public void sum(double a, double b) {
+		double sum = a + b;
+
+		System.out.print(sum);
+	}
+
+}
